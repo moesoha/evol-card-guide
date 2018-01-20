b49893ceb34d3bd4f0427ef8c881a5ff16793c34
Soha Jin <soha@lohu.info>
2018-01-20 22:42:45 +0800

    add a simple top nav

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+	<div id="app">
+		<div>
+			<p>
+				羁绊 | <router-link to="/card/search">搜索</router-link>
+			</p>
+		</div>
+		<hr>
+		<router-view/>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+	name: 'App'
 }
 </script>
 
