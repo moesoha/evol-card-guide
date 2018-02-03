8b66100cc22685d6c6cde4645ef4f32a6d638d90
Soha Jin <soha@lohu.info>
2018-02-03 17:05:03 +0800

    fix bug

--- src/components/card/search.vue
+++ src/components/card/search.vue
@@ -11,7 +11,7 @@
 		<hr>
 		<div id="cardsearch-result" v-if="data.length>0">
 			<div v-for="item in data">
-				<img :src="appConfig.path.getImagePath('card_s',(options.showEvolved?item.pic.evolved:item.pic.normal))">
+				<img :src="appConfig.path.getImagePath('card_s',(showEvolved?item.pic.evolved:item.pic.normal))">
 				<p>
 					[{{item.rare}}] <router-link v-bind:to="'/card/show/'+item.id">{{item.name}}</router-link>
 				</p>
